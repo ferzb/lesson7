a1fe3e1909371d0c9b65e9fe6066301f52074584
Vitaliy Vorynka <vorynkavetal@gmail.com>
2017-06-13 21:40:43 +0300

    2

--- js.js
+++ js.js
@@ -1,17 +1,59 @@
-$.getJSON('users.json', function(data){
+// $.getJSON('https://jsonplaceholder.typicode.com/posts', function(data){
 
- $.each(data, function(i, data){
- 	var name = data.name,
-	 	age = data.age,
-	 	country = data.country,
-	 	city = data.city;
+//  $.each(data, function(i, data){
+//  	$('ul').append('<div><p class="title">'+data.title+'</p><p class="body">'+data.body+'</p></div>')
+//  })
 
-	var arr = [name, age, country, city];
+// });
 
-	$.each(arr, function(i, data){
-		if(data.city !== undefined){
-			alert(data.city); 
-		}
-	})
- })
-});
+// $.ajax({
+// 	method: 'GET',
+// 	url: 'https://jsonplaceholder.typicode.com/posts',
+// 	dataType: 'json'
+// }).done(function(data){
+// 	console.log(data);
+// })
+
+$('#postForm').submit(function(e){
+	e.preventDefault();
+	alert('yes');
+	var title = $('#title').val();
+	var body = $('#body').val();
+	var url = $(this).attr('action');
+
+	$.post(url, {title: title, body: body}).done(function(data){
+		console.log('Post saved');
+		console.log(data);
+	});
+	$.ajax({
+	  type: "GET",
+	  url: url,
+	  data: {title: title, body: body},
+	  success: function(data) {
+	  	console.log(data);
+	  }
+	});
+
+	$.ajax({
+	  type: "POST",
+	  url: url,
+	  data: {title: title, body: body},
+	  success: function(data) {
+	  	console.log(data);
+	  }
+	});
+
+
+
+})
+
+// $('.inner').click(function (event) {
+// 	alert('inner');
+// 	// event.preventDefault();
+// 	// event.stopPropagation();
+// 	return false;
+// })
+// $('.outer').click(function () {
+// 	alert('outer')
+
+// })
